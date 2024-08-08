ce8cad9ec302eb8a8620b4dbfc0c672b177f682b
Nixon <kiplangatnixon28@gmail.com>
2024-08-08 14:47:27 +0300

    uses export and import

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import About from "../components/About";
+import Home from "../components/Home";
+import NavBar from "../components/NavBar";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Home />
+    <div className="App">
       <About />
-    </div>
+      <Home />
+      <NavBar />
+       </div>
   );
 }
 
